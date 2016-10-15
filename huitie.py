69a6c59711e02bafe8ce0b50a04ccfac5e0cc458
littlepinecone <sgflgg@gmail.com>
2016-10-15 16:44:30 +0800

    Update huitie.py

--- huitie.py
+++ huitie.py
@@ -21,14 +21,13 @@ def str2dic(text):
 
 
 def reply(reply_text, tid):
-    huitie_data_text = 'ie=utf-8&kw=华中科技大学&fid=817&tid=4811894587&vcode_md5=&floor_num=1&rich_text=1&tbs=227639f4c9b54eb21476459068&content=12345&files=[]&mouse_pwd=104,105,105,119,104,108,99,107,82,106,119,107,119,106,119,107,119,106,119,107,119,106,119,107,119,106,119,107,82,98,110,111,98,82,106,104,109,109,119,108,109,99,14764590727910&mouse_pwd_t=1476459072791&mouse_pwd_isclick=0&__type__=reply'
+    huitie_data_text = 'ie=utf-8&kw=华中科技大学&fid=817&tid=4811894587&vcode_md5=&floor_num=1&rich_text=1&tbs=1234567&content=12345&files=[]&mouse_pwd=104,105,105,119,104,108,99,107,82,106,119,107,119,106,119,107,119,106,119,107,119,106,119,107,119,106,119,107,82,98,110,111,98,82,106,104,109,109,119,108,109,99,14764590727910&mouse_pwd_t=1476459072791&mouse_pwd_isclick=0&__type__=reply'
     huitie_data = str2dic(huitie_data_text)
     huitie_data['content'] = reply_text
     huitie_data['tid'] = tid
-    huitie_data['tbs'] = '8a8a0c9478bab88e1476496767'
+    huitie_data['tbs'] = '这个地方放你的tbs'
     c_cookies = {
-        # 'BDUSS': 'XlYR3BpYXdmcXk2OWNWM0FFZG1DdXg4U0VVTkpBS09ubEpxfnN0TmdtZWNFeDlZQVFBQUFBJCQAAAAAAAAAAAEAAADL7qYsv7S8-7zTvNO89bz1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJyG91echvdXb'}
-        'BDUSS': 'EZNdkNkS1ZHNExqN1hPamNsMVVEVjExelBhV3dtSTYzLUlpWlpTcEUwbGlHaWxZSVFBQUFBJCQAAAAAAAAAAAEAAACRZIqCuf6wybv6xvfIywAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGKNAVhijQFYd'}
+         'BDUSS': '这个地方放你的BDUSS'}
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
         'Referer': 'http://tieba.baidu.com/p/4811894587'}
