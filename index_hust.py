9f3c9f3022d2ff6216af3b4d554d642f83aa0ba9
小松果 <sgflgg@gmail.com>
2017-02-05 08:24:16 +0800

    图片回复

--- index_hust.py
+++ index_hust.py
@@ -1,18 +1,19 @@
 # /var/lib/openshift/5801a3077628e14abb000135/app-root/runtime/repo
 # coding=utf-8
+
 from bs4 import BeautifulSoup
 import re
 import requests
 import json
 import sys
-# import login
 import tuling_api
 import huitie
 import time
+# import xiaoice
+import bing_api
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
 
 r = requests.get("http://tieba.baidu.com/f?kw=华中科技大学&fr=index")
 # print (r.text)
@@ -20,7 +21,7 @@ page = BeautifulSoup(r.text, "html.parser")
 
 li_list = page.find_all('li', attrs={'class': re.compile("( j_thread_list clearfix)")})
 
-
+# tag去掉无用部分
 before = "'>\n"
 after = "data-field='"
 title_before = '">'
@@ -35,30 +36,50 @@ for t_list in li_list:
     id = t_id["id"]
     reply_num = t_id["reply_num"]
     # print t_id
-
-    # if author_name == "看见加加减减" and reply_num == 0:
-    #    print "看见加加减减",
+    # print reply_num
+    # if author_name == "华科大粽子" and reply_num == 0:
+    #    print "华科大粽子",
     #    print id
-    if reply_num == 0 and a.find("video") == -1:
-        # author_name == "///":
-        # print "///",
+    # a.find("video") == -1当初为了屏蔽视频广告贴,现在好像没有了
+    if author_name == "看见加加减减" and a.find("video") == -1 and t_id != 4069983952:
+        
+        # print author_name
         print '贴号:' + str(id) + ',' + str(reply_num)
         # login.post()
-
+        # 把标题扣出来
         title_match = "/p/" + str(id)
         # print title_match
         title = page.find_all('a', attrs={'href': re.compile(title_match)})
-        # 只有一个元素的数组
+
         title = str(title[0])
         title = title[:title.index(title_before)]
         title = title[title.index(title_after) + len(title_after):]
         print '<' + title
-        t_text = tuling_api.post_text(title)
-        print '>' + t_text
-        huitie.reply(t_text, id)
-        time.sleep(10)
 
-        # print content
-        # if reply_num == 0:
-        #    print "reply_0",
-        #    print id
+        # if "晚安" in title and author_name == "菱夕浣":
+        #    t_text = "晚安男票么么哒!"
+        # else:
+        #    t_text = tuling_api.post_text(title)
+        #    # t_text = xiaoice.xiaoice(title)
+        (t_text, kwd) = tuling_api.post_text(title)
+        # 如果图灵api返回有图片360搜索结果就以关键字返回bing_api的图片
+        if "帮你找到" in t_text:
+
+            (t_text, img_url, x, y) = bing_api.getbingimg(kwd)
+            # 560是百度的限制
+            if x > 560:
+                y = 560 * y / x
+                x = 560
+            x = str(x)
+            y = str(y)
+            # 图片回复
+            huitie.reply('[img pic_type=1 width=' + x + ' ' + 'height=' + y + ']' + img_url + '[/img]' + '[br][br]' + t_text, id)
+        else:
+            # print '>' + t_text
+            huitie.reply(t_text, id)
+            time.sleep(7)
+
+            # print content
+            # if reply_num == 0:
+            #    print "reply_0",
+            #    print id
